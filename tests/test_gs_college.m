a4e30880ee767a3718d818dbb296ebbe17782d20
Sergey Lychagin <39955572+lychagins@users.noreply.github.com>
2018-12-10 15:19:27 +0100

    A test for weighted DA.

--- tests/test_gs_college.m
+++ tests/test_gs_college.m
@@ -30,10 +30,15 @@ college_pref = uint64(college_pref - 1);
 n_acceptable = uint64(num_students*ones(num_colleges, 1));
 college_type = uint64((1:num_colleges)' - 1);
 quota = ones(num_colleges, 1, 'uint64');
+weights = ones(num_students, 1, 'uint64');
 util_outside = min(student_utils, [], 2) - 1;
+
 
 act_engagements = gs_college_opt(college_pref, n_acceptable, ...
     college_type, quota, student_utils, util_outside);
+
+act_engagements2 = gs_college_opt(college_pref, n_acceptable, ...
+    college_type, quota, student_utils, util_outside, weights);
 
 % Type test
 assert(isa(act_engagements, 'integer'), 'Non-integer engagements vector.');
@@ -46,4 +51,9 @@ assert(all(size(act_engagements) == [num_students, 1]), ...
 exp_engagements = csvread('resources/gs_engagements.csv');
 assert(all(act_engagements == exp_engagements),...
     'Simulated placements do not match expected results');
+
+% Weighted DA with unit weights is equivalent to the standard DA
+assert(all(act_engagements == act_engagements2), ...
+    'Weighted DA with unit weights is not equivalent to the standard DA');
+
 disp('College-optimal Gale-Shapley: PASSED');
